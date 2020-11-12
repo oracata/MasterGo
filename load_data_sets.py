b8238ae9ade9dd30844d5658da2044b5ed4fce8a
oracata <50257800+oracata@users.noreply.github.com>
2020-11-12 09:16:01 +0800

    Initial commit

---
+++ load_data_sets.py
@@ -0,0 +1,106 @@
+#-*- coding:gbk -*-
+#from features import bulk_extract_features
+import numpy as np
+import os
+from sgf_wrapper import replay_sgf   #wrapper 封装
+import sys
+import itertools
+import tqdm
+import gzip
+import go
+import utils
+import struct
+
+from features import bulk_extract_features,make_onehot
+
+CHUNK_HEADER_FORMAT = "iii?"
+
+
+def make_onehot(coords):
+    num_positions = len(coords)
+    output = np.zeros([num_positions, go.N ** 2], dtype=np.uint8)
+    for i, coord in enumerate(coords):
+        output[i, utils.flatten_coords(coord)] = 1
+    return output
+
+def find_sgf_files(*dataset_dirs): #python因为是脚本语言而不是编译语言，所以函数要先定义后才能调用 
+    for dataset_dir in dataset_dirs:
+        full_dir=os.path.join(os.getcwd(),dataset_dir)
+        dataset_files=[os.path.join(full_dir,name) for name in os.listdir(full_dir)] #取得所有文件的全路径名，os.listdir()得到目录下所有文件名或目录名
+                                                                                     #os.listdir
+                                                                                     # 小括号( )：代表tuple元组数据类型
+                                                                                     # 中括号[ ]：代表list列表数据类型
+                                                                                     # 大括号{ }花括号：代表dict字典数据类型 字典是由键对值组组成。冒号':'分开键和值，逗号','隔开组
+                                                                                     #在 python 中，strings, tuples, 和 numbers 是不可更改的对象，而 list,dict 等则是可以修改的对象。
+        for f in dataset_files:
+            if os.path.isfile(f) and f.endswith(".sgf"):   #判断文件是否正确   ,判断字符串是否以指定后缀结尾
+               yield f                                      #生成器让数据一步一步地处理，避免一次数据量过大而撑爆内存
+
+
+def get_positions_from_sgf(file):     #取得行棋位置
+    with open(file) as f:                                   #打开一个文件到内存
+         for position_w_context in replay_sgf(f.read()):   #得到行棋位置
+             if position_w_context.is_usable():
+                 yield position_w_context
+
+class DataSet(object):   #类名的单词首字母大写 ，类继承了object类的属性 为什么要继承object类呢？目的是便于统一操作。继承object类是为了让自己定义的类拥有更多的属性。比如__init__()
+    def __init__(self,pos_features,next_moves,results,is_test=False):      #self代表当前对象的地址，self能避免非限定调用的全局变量。
+        #初始化属性
+        self.pos_features = pos_features
+        self.next_moves = next_moves
+        self.results = results
+        self.is_test = is_test
+        assert pos_features.shape[0]==next_moves.shape[0],"在下一步中出现相同的数不要丢掉"  # 用于判断一个表达式，在表达式条件为 false 的时候触发异常 避免运行后出现崩溃的情况。
+        self.data_size = pos_features.shape[0]
+        self.board_size = pos_features.shape[1]
+        self.input_planes = pos_features.shape[-1]
+        self._index_within_epoch = 0
+        self.shuffle()
+
+    def shuffle(self):
+        perm = np.arange(self.data_size)
+        np.random.shuffle(perm)
+        self.pos_features = self.pos_features[perm]
+        self.next_moves = self.next_moves[perm]
+        self._index_within_epoch = 0
+
+    def write(self, filename):
+        header_bytes = struct.pack(CHUNK_HEADER_FORMAT, self.data_size, self.board_size, self.input_planes, self.is_test)
+        position_bytes = np.packbits(self.pos_features).tostring()
+        next_move_bytes = np.packbits(self.next_moves).tostring()
+        with gzip.open(filename, "wb", compresslevel=6) as f:
+             f.write(header_bytes)
+             f.write(position_bytes)
+             f.write(next_move_bytes)
+
+       #如果
+    @staticmethod    #这个注解表示  不用实例化类 可直接使用这个方法
+    def from_positions_w_context(positions_w_context, is_test=False):
+        positions, next_moves, results = zip(*positions_w_context)    #将元组列表解压成矩阵
+        extracted_features = bulk_extract_features(positions)
+        encoded_moves = make_onehot(next_moves)
+        return DataSet(extracted_features, encoded_moves, results, is_test=is_test)
+
+def split_test_training(positions_w_context,est_num_positions):
+    desired_test_size=10**5
+    if est_num_positions<2*desired_test_size:                        #1000张棋谱才能充分训练
+       print("！！！没有足够的数据生成测试数据集. Splitting 67:33")
+       positions_w_context = list(tqdm.tqdm(positions_w_context))
+       test_size = len(positions_w_context) // 3
+       return positions_w_context[:test_size], [positions_w_context[test_size:]]
+    else:
+       print("Estimated number of chunks: %s" % (
+             (est_num_positions - desired_test_size) // CHUNK_SIZE), file=sys.stderr)
+       test_chunk = take_n(desired_test_size, positions_w_context)
+       training_chunks = iter_chunks(CHUNK_SIZE, positions_w_context)
+       return test_chunk, training_chunks
+    pass
+
+def parse_data_sets(*datasets):  #解析数据
+    print("在下面列文件路径{}中查找sfg文件".format('\n'.join(datasets)))          #format:替换{}的内容，join:每个文件中间插入一个换行符 换行符是单引号
+    sgf_files=list(find_sgf_files(*datasets))   #得到全路径文件名列表，list() 方法用于将元组转换为列表 
+    print("共有%s 个sgf文件"%len(sgf_files),file=sys.stderr)   #错误输出到
+    est_num_positions=len(sgf_files)*200 #估计每局手数 estimate：估计
+    positions_w_context=itertools.chain(*map(get_positions_from_sgf,sgf_files))#把所有文件组合起来。生成迭代内容，chain()可以把一组迭代对象串联起来，形成一个更大的迭代器
+    test_chunk,training_chunks=split_test_training(positions_w_context,est_num_positions)#分割文件
+    return test_chunk,training_chunks
---
+++ sgf_wrapper.py
@@ -0,0 +1,100 @@
+#-*- coding:gbk -*-
+'''
+#块注释用3个单引号
+代码功能：从sgf文件中提取手数和它们的下一手
+2个主要功能：
+'''
+
+import sgf
+import go
+from go  import  Position
+from collections import namedtuple
+from utils import parse_sgf_coords as pc
+
+
+class GameMetadata(namedtuple("GameMetadata", "result handicap board_size")):   #构造类 namedtuple类位于collections模块,有了namedtuple后通过属性访问数据能够让我们的代码更加的直观更好维护
+        pass
+
+class PositionWithContext(namedtuple("SgfPosition", "position next_move metadata")):
+    '''
+    Wrapper around go.Position.
+    Stores a position, the move that came next, and the eventual result.
+    '''
+    def is_usable(self):
+        return all([
+            self.position is not None,
+            self.next_move is not None,
+            self.metadata.result != "Void",
+            self.metadata.handicap <= 4,
+        ])
+
+    def __str__(self):
+        return str(self.position) + '\nNext move: {} Result: {}'.format(self.next_move, self.result)
+
+
+def sgf_prop(value_list):
+    '''取sgf属性元素值，把sgf文件转换为合理的值'''
+    if value_list is None:
+        return None
+    if len(value_list) == 1:
+        return value_list[0]
+    else:
+        return value_list
+def handle_node(pos, node):
+    'A node can either add B+W stones, play as B, or play as W.'
+    props = node.properties
+    black_stones_added = [pc(coords) for coords in props.get('AB', [])]
+    white_stones_added = [pc(coords) for coords in props.get('AW', [])]
+    if black_stones_added or white_stones_added:
+        return add_stones(pos, black_stones_added, white_stones_added)
+    # If B/W props are not present, then there is no move. But if it is present and equal to the empty string, then the move was a pass.
+    elif 'B' in props:
+        black_move = pc(props.get('B', [''])[0])
+        return pos.play_move(black_move, color=go.BLACK)
+    elif 'W' in props:
+        white_move = pc(props.get('W', [''])[0])
+        return pos.play_move(white_move, color=go.WHITE)
+    else:
+        return pos
+
+def get_next_move(node):
+    if not node.next:
+        return None
+    props = node.next.properties
+    if 'W' in props:
+        return pc(props['W'][0])
+    else:
+        return pc(props['B'][0])
+
+def maybe_correct_next(pos, next_node):
+    if next_node is None:
+        return
+    if (('B' in next_node.properties and not pos.to_play == go.BLACK) or
+        ('W' in next_node.properties and not pos.to_play == go.WHITE)):
+        pos.flip_playerturn(mutate=True)
+
+def replay_sgf(sgf_contents):
+    '''
+    '''
+    collection=sgf.parse(sgf_contents) #collection能把数据对象化 便于操作
+    game=collection.children[0]
+    prop=game.root.properties
+    assert int(sgf_prop(prop.get('GM',['1'])))==1,"这不是围棋棋谱！"   #prop.get  取到一个元素  字符转数字
+    komi=0                                                             #初始化贴子规则
+    if prop.get('KM') != None:
+        komi=float(sgf_prop(prop.get('KM')))
+    metadata=GameMetadata(
+            result=sgf_prop(prop.get('RE')),                            #比赛结果
+            handicap=int(sgf_prop(prop.get('HA', [0]))),               #让子
+            board_size=int(sgf_prop(prop.get('SZ')))                    #棋盘大小
+            )
+    go.set_board_size(metadata.board_size)
+    pos=Position(komi=komi)
+    current_node=game.root
+    while pos is not None and current_node is not None :                               #开始遍历棋谱节点
+          pos = handle_node(pos, current_node)
+          maybe_correct_next(pos, current_node.next)
+          next_move = get_next_move(current_node)
+          yield PositionWithContext(pos, next_move, metadata)
+          current_node = current_node.next
+    pass
